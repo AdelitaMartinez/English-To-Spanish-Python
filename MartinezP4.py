0059c72890567e89db46cd11ca7386723e2c7ae4
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-06 15:22:18 -0600

    first commit

---
+++ MartinezP4.py
@@ -0,0 +1,36 @@
+# MartinezP4
+# Programmer: Adelita Martinez
+# Email: amartinez1013@cnm.edu
+# Purpose: Translate common phrases
+# Python version: 3.12.3
+
+#http://www.learnspanishtoday.com/learning_module/grammar.htm
+english2spanish={'Good morning.':'Buenos días.',
+'Good afternoon.':'Buenas tardes.',
+'Good evening. (greeting)':'Buenas noches.',
+'Hello, my name is John.':'Hola, me llamo Juan.',
+'What is your name?':'¿Cómo se llama usted?',
+'How are you?':'¿Cómo está usted?',
+'I am fine.':'Estoy bien.',
+'Nice to meet you.':'Mucho gusto.',
+'Goodbye.':'Adiós.',
+'See you later.':'Hasta luego.',
+'I am lost. Where is the restroom?':'Estoy perdido. ¿Dónde está el baño?',
+'Excuse me.':'Con permiso. OR Perdóname',
+'Please.':'Por favor.',
+'Thank you.':'Gracías.',
+'Bless you.':'Salud.',
+'You are welcome (it was nothing).':'De nada.',
+'How much does it cost?':'¿Cuánto cuesta?',
+'How many are there?':'¿Cuántos hay?',
+'There are many.':'Hay muchos.',
+'Do you want to buy this?':'¿Quiere comprarlo usted?',
+'What time is it?':'¿Qué hora es?',
+'How do you say maybe in Spanish?':'¿Cómo se dice maybe en Español?',
+'Yes.':'Sí.',
+'No.':'No.',
+'I do not understand.':'Yo no comprendo.',
+'Would you speak slower, please.':'Por favor, habla mas despacio.',
+'Who?':'¿Quièn?',
+'Why?':'¿Por què?'}
+
